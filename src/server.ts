ea4ce8f99b0417591721d3d4863989a75f0a0dca
Lucas de Almeida Schmid <lucas.schmid@MJV.net>
2024-06-15 13:34:57 -0300

    feature: Iniciado estrutura de pastas e instalação de dependencias

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+const PORT = process.env.PORT || 3000;
+
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
