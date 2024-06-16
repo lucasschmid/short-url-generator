6056f2c1cebec2c73da9f4ba6c336affaede4567
Lucas de Almeida Schmid <lucas.schmid@MJV.net>
2024-06-16 17:52:07 -0300

    fix: removido dotenv do server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-dotenv.config();
 import { sequelize } from './models/index.model';
 import authRoutes from './routes/auth.routes';
 import urlRoutes from './routes/url.routes';
